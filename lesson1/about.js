26db65dd68b1a82bf420dad9e8629ea6c0d76ae4
hung1720 <coderlazy17@gmail.com>
2024-05-31 22:17:04 +0700

    finish lesson 1

---
+++ lesson1/about.js
@@ -0,0 +1,11 @@
+let x, y, z;
+x = 5;
+y = 6;
+z = x + y;
+
+document.getElementById("demojs").innerHTML = "The value of z is " + z + ".";
+
+function myFunction() {
+  document.getElementById("demo1").innerHTML = "Hello demo 1";
+  document.getElementById("demo2").innerHTML = "Hello demo 2";
+}
